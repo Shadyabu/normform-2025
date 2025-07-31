bce8b79d6bf44989d0b66ae17f93f62b7ccdc421
Monikanavapariya9 <monikanavapariya9@gmail.com>
2025-07-31 23:08:29 +0530

    Checkout

--- next/src/components/shop/BuyNowButton.jsx
+++ next/src/components/shop/BuyNowButton.jsx
@@ -1,123 +1,188 @@
-'use client';
-import { useState, useEffect } from 'react';
+import { useSiteGlobals } from '@/utils/SiteGlobalsContext';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import BuyNowButtonOptions from './BuyNowButtonOptions';
+import BuyNowButtonColourOptions from './BuyNowButtonColourOptions';
 
-const STOREFRONT_API_TOKEN = 'bb705fb46bc52390bbdc47e8474c0877';
-const SHOP_DOMAIN = 'norm-form.myshopify.com';
-const API_ENDPOINT = `https://${SHOP_DOMAIN}/api/2024-07/graphql.json`;
-const PRODUCT_HANDLE = 'notso'; // Change to your product handle
+const BuyNowButton = ({ shopifyId, product, }) => {
 
-const BuyNowButton = () => {
-  const [variantId, setVariantId] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const container = useRef();
+  const { shopifyUI, shopifyCart, setCartIsOpen, currency, } = useSiteGlobals();
 
-  // Step 1: Fetch variantId
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const res = await fetch(API_ENDPOINT, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Shopify-Storefront-Access-Token': STOREFRONT_API_TOKEN,
-        },
-        body: JSON.stringify({
-          query: `
-            query GetProductByHandle($handle: String!) {
-              productByHandle(handle: $handle) {
-                title
-                variants(first: 1) {
-                  edges {
-                    node {
-                      id
-                    }
-                  }
-                }
-              }
-            }
-          `,
-          variables: {
-            handle: PRODUCT_HANDLE,
-          },
-        }),
-      });
+  const [ isRendered, setIsRendered ] = useState(false);
+  const isRenderedRef = useRef();
 
-      const json = await res.json();
-      const id = json?.data?.productByHandle?.variants?.edges?.[0]?.node?.id;
-      setVariantId(id);
-    };
+  const [ variantChanges, setVariantChanges ] = useState(0);
 
-    fetchProduct();
-  }, []);
+  const [ variants, setVariants ] = useState([]);
 
-  // Step 2: Handle Buy Now
-  const handleBuyNow = async () => {
-    console.log('variantId',variantId)
-    if (!variantId) {
-      alert('Variant not ready');
-      return;
+  const productVariants = useMemo(() => {
+    if (product?.variants?.length > 0) {
+      return product.variants;
     }
+    return [];
+  }, [ product ]);
 
-    setLoading(true);
-
-    const res = await fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': STOREFRONT_API_TOKEN,
-      },
-      body: JSON.stringify({
-        query: `
-          mutation CartCreate($input: CartInput!) {
-            cartCreate(input: $input) {
-              cart {
-                id
-                checkoutUrl
-              }
-              userErrors {
-                field
-                message
-              }
-            }
+  const activeVariant = useMemo(() => {
+    if (productVariants?.length > 0) {
+      let productVariant = null;
+      for (let i = 0; i < productVariants.length; i++) {
+        let isMatch = true;
+        const options = [ productVariants[ i ].option1, productVariants[ i ].option2, productVariants[ i ].option3 ];
+        for (let variant of variants) {
+          if (!options.includes(variant.active)) {
+            isMatch = false;
           }
-        `,
-        variables: {
-          input: {
-            lines: [
-              {
-                quantity: 1,
-                merchandiseId: variantId,
-              },
-            ],
-          },
-        },
-      }),
-    });
-
-    const json = await res.json();
-    setLoading(false);
-
-    const errors = json?.data?.cartCreate?.userErrors;
-    if (errors && errors.length > 0) {
-      alert('Error: ' + errors[0].message);
-      return;
-    }
-
-    const checkoutUrl = json?.data?.cartCreate?.cart?.checkoutUrl;
-    console.log('checkoutUrl',checkoutUrl)
-    if (checkoutUrl) {
-    //  window.location.href = checkoutUrl;
+        }
+        if (isMatch) {
+          productVariant = productVariants[ i ].id;
+        }
+      }
+      return productVariant;
     } else {
-      alert('Failed to create cart');
+      return null;
     }
-  };
+  }, [ variants, productVariants ]);
+
+  useLayoutEffect(() => {
+    let raf;
+
+    const setupButton = () => {
+      let node = container.current;
+      if (!node) {
+        raf = requestAnimationFrame(setupButton);
+      } else {
+        container.current.innerHTML = '';
+
+        if (!shopifyUI) return;
+
+        if (node && shopifyUI && shopifyId && shopifyUI?.components?.cart?.length > 0) {
+          shopifyUI.createComponent('product', {
+            id: shopifyId.toString(),
+            variantId: activeVariant,
+            node,
+            moneyFormat: `${ currency.symbol }{{amount_no_decimals}}`,
+            options: {
+              product: {
+                iframe: false,
+                contents: {
+                  img: false,
+                  title: false,
+                  price: false
+                },
+                text: {
+                  button: 'Add to Cart'
+                },
+                order: [
+                  'options',
+                  'variantTitle',
+                  'price',
+                  'description',
+                  'button',
+                ],
+                selectedOptions: {
+                  Color: 'black',
+                  Size: 'md'
+                },
+                events: {
+                  addVariantToCart: function (product) {
+                    setCartIsOpen(true);
+                  },
+                  afterRender: function (component) {
+                    const variantsArray = [];
+                    if (component.node) {
+                      const variantNodes = component.node.querySelectorAll('.shopify-buy__option-select');
+                      if (variantNodes.length > 0) {
+                        variantNodes.forEach((node) => {
+                          const values = [];
+                          const select = node.querySelector('select');
+                          const name = select.getAttribute('name').toLowerCase();
+                          const options = select.querySelectorAll('option');
+
+                          options.forEach((option) => {
+                            values.push({
+                              value: option.getAttribute('value'),
+                              text: option.textContent
+                            });
+                          });
+
+                          variantsArray.push({
+                            name,
+                            values,
+                            select,
+                            active: values[ 0 ].value,
+                          });
+                        });
+                      }
+                    }
+                    if (!isRenderedRef.current) {
+                      setVariants(variantsArray);
+                      setIsRendered(true);
+                      isRenderedRef.current = true;
+                    }
+                  },
+                  afterUpdateConfig: function (component) {
+                  },
+                  DOMEvents: {
+                    'click .option-input': function (event) {
+                    }
+                  },
+                },
+              },
+              toggle: {
+                iframe: false,
+                contents: {
+                  title: false,
+                  price: false,
+                  img: false,
+                  button: 'Close'
+                }
+              },
+            }
+          });
+        }
+      }
+    }
+
+    setupButton();
+
+    return () => {
+      cancelAnimationFrame(raf);
+    }
+  }, [ shopifyId, shopifyUI, shopifyUI?.components?.cart, shopifyCart, setCartIsOpen, currency.symbol, activeVariant, ]);
+
+  useEffect(() => {
+    if (variants?.length > 0 && isRendered) {
+      for (let i = 0; i < variants?.length; i++) {
+        const variant = variants[ i ];
+        if (variant.active && variant.select) {
+          const select = variant.select;
+          const options = select.querySelectorAll('option');
+          options.forEach((option) => {
+            if (option.getAttribute('value') === variant.active) {
+              option.selected = true;
+            }
+          });
+        }
+      }
+    }
+  }, [ variants, isRendered, ]);
 
   return (
-    <button
-      onClick={handleBuyNow}
-      disabled={loading || !variantId}
-      className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
-    >
-      {loading ? 'Processing...' : 'Buy Now'}
-    </button>
+    <div className=''>
+      {
+        variants?.map((variant, index) => (
+          (
+            variant.name.toLowerCase() === 'color' ||
+            variant.name.toLowerCase() === 'colors' ||
+            variant.name.toLowerCase() === 'colour' ||
+            variant.name.toLowerCase() === 'colours') ?
+          <BuyNowButtonColourOptions key={ index } { ...{ variant, variants, setVariants, variantChanges, setVariantChanges, } } />
+          :
+          <BuyNowButtonOptions key={ index } { ...{ variant, variants, setVariants, variantChanges, setVariantChanges, } } />
+        ))
+      }
+      <div className='' ref={ container } />
+    </div>
   );
 };
 
