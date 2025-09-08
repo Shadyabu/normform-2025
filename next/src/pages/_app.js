07ec9ebf64662362494c14f6c6aec69172f9c355
Shady Abushady <sayabushady@gmail.com>
2025-09-08 12:14:03 -0400

    tag manager

--- next/src/pages/_app.js
+++ next/src/pages/_app.js
@@ -1,13 +1,27 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import { SiteGlobalsProvider } from '@/utils/SiteGlobalsContext';
 import Layout from './_layout';
-
+import Script from 'next/script'; 
 export default function App({ Component, pageProps }) {
   return (
-    <SiteGlobalsProvider>
-      <Layout>
-        <Component { ...pageProps } />
-      </Layout>
-    </SiteGlobalsProvider>
-  )
+    <>
+      {/* Google Tag Manager script – fires after page is interactive */}
+      <Script 
+        id="gtm-script" 
+        strategy="afterInteractive" 
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:''; j.async=true; j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl; f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5T9KBN6L');`
+        }} 
+      />
+      <SiteGlobalsProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SiteGlobalsProvider>
+    </>
+  );
 }
--- next/src/pages/_document.js
+++ next/src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {/* Google Tag Manager noscript fallback – renders when JS is disabled */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T9KBN6L"
+                     height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
